9ef3e700535ef91149ea26ab673e73916711df2b
Andrew Mason <andrewmasonmedia@gmail.com>
2020-01-17 11:11:29 -0500

    Deleted metricdashboard mockup page, update to mobile menu styles, copied mobile menu to detail and about pages

--- dist/warren_site.e31bb0bc.js
+++ dist/warren_site.e31bb0bc.js
@@ -19871,7 +19871,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56669" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50223" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
